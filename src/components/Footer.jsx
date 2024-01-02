5f0def4538d5ecb0b3af4ff9ea408dba8608ca85
itzaadi <asutariya9879@gmail.com>
2024-01-03 01:16:05 +0530

    one

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>
+                Techstart
+            </h1>
+            <p>
+                @all right reserved
+            </p>
+        </div>
+
+        <div>
+            <h5>follow us</h5>
+            <div>
+                <a href="www.youtube.com">Youtube</a>
+                <a href="www.youtube.com">Instagram</a>
+                <a href="www.youtube.com">Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
